9a1164a8ca45bae8806bd70755a918681aba4cd0
User121416 <108672206+Allkrivtim@users.noreply.github.com>
2024-10-20 19:40:59 +0300

    0.0101
    Исправлена проблема отправки options запроса через nginx

--- app/main.py
+++ app/main.py
@@ -4,8 +4,17 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 from handlers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить доступ с любого источника
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (POST, GET, OPTIONS и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 # Подключаем статические файлы
 app.mount("/static", StaticFiles(directory="static"), name="static")
