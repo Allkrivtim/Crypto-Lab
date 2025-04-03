c260d059694b8e7ce86edf2ca0c77ac489bcda58
root <root@feesta009.fvds.ru>
2025-04-03 08:37:19 +0300

    Added server configs

--- site/tests/test_services/main_page.py
+++ site/tests/test_services/main_page.py
@@ -1,11 +1,9 @@
 from app.core.logger import router_logger
 from fastapi import APIRouter
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 
 router = APIRouter()
 
 @router.get("/main_page")
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("pages/index.html", {"request": request})
